81b21f086d37c352ea401fe1679a78bf40f114c7
Anant Narayanan <anant@kix.in>
2013-03-26 10:41:22 -0700

    Import explicit sync collection from @petebacondarwin

--- angularFire.js
+++ angularFire.js
@@ -1,6 +1,10 @@
 'use strict';
 
-angular.module('firebase', []).factory('angularFire', function($q) {
+angular.module('firebase', []).value('Firebase', Firebase);
+
+// Implicit syncing. angularFire binds a model to $scope and keeps the dat
+// synchronized with a Firebase location both ways.
+angular.module('firebase').factory('angularFire', function($q) {
   return function(url, scope, name) {
     var af = new AngularFire($q, url);
     return af.associate(scope, name);
@@ -32,7 +36,7 @@ AngularFire.prototype = {
         }
       }
       self._safeApply($scope,
-          self._resolve.bind(self, $scope, name, resolve, self._remoteValue));
+        self._resolve.bind(self, $scope, name, resolve, self._remoteValue));
     });
     return deferred.promise;
   },
@@ -68,3 +72,137 @@ AngularFire.prototype = {
     }
   }
 };
+
+// Explicit syncing. Provides a collection object you can modify.
+// Original code from https://github.com/petebacondarwin/angular-firebase/blob/master/ng-firebase-collection.js
+angular.module('firebase').factory('angularFireCollection', function($timeout) {
+  /**
+   * @class item in the collection
+   * @param {DataSnapshot} ref      reference to the firebase data snapshot for this item
+   * @param {int} index             position of the item in the collection
+   *
+   * @property {DataSnapshot} $ref  reference to the firebase data snapshot for this item
+   * @property {String} $id         unique identifier for this item within the collection
+   * @property {int} $index         position of the item in the collection
+   */
+  function angularFireItem(ref, index) {
+    this.$ref = ref.ref();
+    this.$id = ref.name();
+    this.$index = index;
+    angular.extend(this, ref.val());
+  }
+
+  /**
+   * create a firebaseCollection
+   * @param  {String} collectionUrl The firebase url where the collection lives
+   * @return {Array}                An array that will hold the items in the collection
+   */
+  return function(collectionUrl) {
+    var collection = [];
+    var indexes = {};
+    var collectionRef = new Firebase(collectionUrl);
+
+    function getIndex(prevId) {
+      return prevId ? indexes[prevId] + 1 : 0;
+    }
+    
+    function addChild(index, item) {
+      indexes[item.$id] = index;
+      collection.splice(index,0,item);
+      console.log('added: ', index, item);
+    }
+
+    function removeChild(id) {
+      var index = indexes[id];
+
+      // Remove the item from the collection
+      collection.splice(index, 1);
+      indexes[id] = undefined;
+
+      console.log('removed: ', id);
+    }
+
+    function updateChild (index, item) {
+      collection[index] = item;
+      console.log('changed: ', index, item);
+    }
+
+    function moveChild (from, to, item) {
+      collection.splice(from, 1);
+      collection.splice(to, 0, item);
+      updateIndexes(from, to);
+      console.log('moved: ', from, ' -> ', to, item);
+    }
+
+    function updateIndexes(from, to) {
+      var length = collection.length;
+      to = to || length;
+      if ( to > length ) { to = length; }
+      for(index = from; index < to; index++) {
+        var item = collection[index];
+        item.$index = indexes[item.$id] = index;
+      }
+    }
+
+    collectionRef.on('child_added', function(data, prevId) {
+      $timeout(function() {
+        var index = getIndex(prevId);
+        addChild(index, new angularFireItem(data, index));
+        updateIndexes(index);
+      });
+    });
+
+    collectionRef.on('child_removed', function(data) {
+      $timeout(function() {
+        var id = data.name();
+        removeChild(id);
+        updateIndexes(indexes[id]);
+      });
+    });
+
+    collectionRef.on('child_changed', function(data, prevId) {
+      $timeout(function() {
+        var index = indexes[data.name()];
+        var newIndex = getIndex(prevId);
+        var item = new angularFireItem(data, index);
+
+        updateChild(index, item);
+
+        if ( newIndex !== index ) {
+          moveChild(index, newIndex, item);
+        }
+
+      });
+    });
+
+    collectionRef.on('child_moved', function(ref, prevId) {
+      $timeout(function() {
+        var oldIndex = indexes[ref.name()];
+        var newIndex = getIndex(prevId);
+        var item = collection[oldIndex];
+        moveChild(oldIndex, newIndex, item);
+      });
+    });
+
+    collection.$add = function(item) {
+      collectionRef.push(item);
+    };
+    collection.$remove = function(itemOrId) {
+      var item = angular.isString(itemOrId) ? collection[itemOrId] : itemOrId;
+      item.$ref.remove();
+    };
+
+    collection.$update = function(itemOrId) {
+      var item = angular.isString(itemOrId) ? collection[itemOrId] : itemOrId;
+      var copy = {};
+      angular.forEach(item, function(value, key) {
+        if (key.indexOf('$') !== 0) {
+          copy[key] = value;
+        }
+      });
+      item.$ref.set(copy);
+    };
+
+    return collection;
+  };
+});
