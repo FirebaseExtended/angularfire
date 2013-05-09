bbbd01621b2313b8590a81667d8b0e0c874dd41d
Anant Narayanan <anant@kix.in>
2013-05-09 16:49:54 -0700

    Add more tests for angularFire implicit sync

--- tests/test_todo.js
+++ tests/test_todo.js
@@ -7,7 +7,7 @@ casper.start("tests/test_todo.html", function() {
   this.test.assertEval(function() {
     if (!Firebase) return false;
     if (!AngularFire) return false;
-    if (!_scope) return false;
+    if (_scope != null) return false;
     return true;
   }, "AngularFire exists");
 });
@@ -20,23 +20,83 @@ casper.thenEvaluate(function() {
   });
 });
 
-/*
-casper.then(function() {
-  var _testTodo = {
-    completed: false,
-    title: "Eat some Chocolate"
-  };
+casper.waitFor(function() {
+  return this.getGlobal("__flag") === true;
+});
 
-  this.test.assertEval(function(todo) {
-    _scope.newTodo = todo;
+casper.waitFor(function() {
+  return this.evaluate(function() {
+    return _scope != null;
+  });
+});
+
+casper.then(function() {
+  var _testTodo = "Eat some Chocolate";
+
+  this.test.assertEval(function(title) {
+    _scope.newTodo = title;
     _scope.addTodo();
+    _scope.$digest();
     return _scope.newTodo == "";
   }, "Adding a new TODO", _testTodo);
 
   this.waitForSelector(".todoView", function() {
     this.test.assertEval(function(todo) {
       return testIfInDOM(todo, document.querySelector(".todoView"));
-    }, "Testing if TODO is in the DOM", _testTodo);
+    }, "Testing if TODO is in the DOM", {title: _testTodo, completed: false});
+  });
+});
+
+casper.then(function() {
+  this.evaluate(function() {
+    _scope.todos[0].completed = true;
+    _scope.$digest();
+  });
+  this.waitFor(function() {
+    return this.evaluate(function() {
+      return document.querySelector(".todoView").childNodes[1].checked === true;
+    });
+  });
+});
+
+casper.then(function() {
+  var _testTodo = "Run for 10 miles";
+
+  this.test.assertEval(function(title) {
+    _scope.todos.push({title: title, completed: false});
+    _scope.$digest();
+    return _scope.newTodo == "";
+  }, "Adding another TODO", _testTodo);
+
+  this.waitFor(function() {
+    return this.evaluate(function() {
+      return document.querySelectorAll(".todoView").length == 2;
+    });
+  });
+});
+
+/*
+casper.then(function() {
+  var _testTodo = "Pick up laundry";
+
+  this.test.assertEval(function(title) {
+    _scope.todos.push({title: title, completed: true});
+    _scope.$digest();
+    return _scope.newTodo == "";
+  }, "Testing if limits and queries work", _testTodo);
+
+  this.evaluate(function() {
+    window.__flag = false;
+    var ref = new Firebase(_url);
+    ref.once("value", function(snapshot) {
+      if (snapshot.val().length == 3) {
+        window.__flag = true;
+      }
+    });
+  });
+
+  this.waitFor(function() {
+    return this.getGlobal("__flag") === true;
   });
 });
 */
