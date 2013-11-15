a77f2903e17ae3ecab0ac6320bbaa93326bc4172
Jeff Cross <middlefloor@gmail.com>
2013-11-14 17:16:38 -0800

    feat(child_added): implement child_added indexing

--- tests/firebase-mock.js
+++ tests/firebase-mock.js
@@ -1,6 +1,12 @@
 var Firebase = function (url) {
   this._url = url;
-  this.on = function () {};
+  this.on = function (event, callback) {
+    this._on = this._on || [];
+    this._on.push(event);
+
+    this._events = this._events || {};
+    this._events[event] = callback;
+  };
 
   this.startAt = function (num) {
     this._startAt = num;
--- tests/unit/omnibinder-protocol.spec.js
+++ tests/unit/omnibinder-protocol.spec.js
@@ -22,17 +22,65 @@ describe('OmniBinder Protocol', function () {
 
 
     it('should call limit if provided in query', function () {
-      var binder = {query: {limit: 10}};
+      var binder = {query: {limit: 10, url: 'foo/bar'}};
       firebinder.subscribe(binder);
       expect(binder.fbRef._limit).toBe(10);
     });
 
 
     it('should call startAt if provided in query', function () {
-      var binder = {query: {limit: 20, startAt: 50}};
+      var binder = {query: {limit: 20, startAt: 50, url: 'foo/bar'}};
       firebinder.subscribe(binder);
       expect(binder.fbRef._limit).toBe(20);
       expect(binder.fbRef._startAt).toBe(50);
+    });
+
+
+    describe('child_added', function () {
+      var binder;
+      beforeEach(function (){
+        binder = {query: {url: 'foo/bar'}};
+      });
+
+
+      it('should call on child_added on the ref during construction',
+        function () {
+          firebinder.subscribe(binder);
+          expect(typeof binder.fbRef._events['child_added']).toBeDefined();
+        });
+
+
+      it('should call onChildAdded on the event of the child being added',
+        function () {
+          var spy = spyOn(firebinder, 'onChildAdded');
+
+          firebinder.subscribe(binder);
+          binder.fbRef._events['child_added']();
+
+          expect(spy).toHaveBeenCalled();
+        });
+
+      it('should insert the child\'s name at the end of the binder index if no prev is provided',
+        function () {
+          firebinder.subscribe(binder);
+          binder.index.push('baz', 'foo');
+          firebinder.onChildAdded.call(firebinder, binder, {val: 'foo', name: function () {
+            return 'bar';
+          }});
+
+          expect(binder.index.indexOf('bar')).toBe(2);
+        });
+
+      it('should insert the child\'s name after the prev in the binder index',
+        function () {
+          firebinder.subscribe(binder);
+          binder.index.push('baz', 'foo');
+          firebinder.onChildAdded.call(firebinder, binder, {val: 'foo', name: function () {
+            return 'bar';
+          }}, 'baz');
+
+          expect(binder.index.indexOf('bar')).toBe(1);
+        });
     });
   })
 });
