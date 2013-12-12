4fc4f1f89bdf5855feb5b4e68eed2bbf28d228c3
Anant Narayanan <anant@kix.in>
2013-12-11 16:54:25 -0800

    Fix master at 0.3.2 before v2 merge

--- angularfire.js
+++ angularfire.js
@@ -5,7 +5,7 @@
 // as normal, except that the changes are also sent to all other clients
 // instead of just a server.
 //
-//      AngularFire 0.3
+//      AngularFire 0.3.2
 //      http://angularfire.com
 //      License: MIT
 
