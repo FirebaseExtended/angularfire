c0ce895272634769eda03d813a10641e76edeee6
David East <david@firebase.com>
2016-02-17 16:45:32 -0800

    firebaseRef in an IIFE

--- src/firebaseRef.js
+++ src/firebaseRef.js
@@ -1,46 +1,49 @@
-"use strict";
+(function() {
+  "use strict";
 
-function FirebaseRef() {
-  this.urls = null;
-  this.registerUrl = function registerUrl(urlOrConfig) {
+  function FirebaseRef() {
+    this.urls = null;
+    this.registerUrl = function registerUrl(urlOrConfig) {
 
-    if (typeof urlOrConfig === 'string') {
-      this.urls = {};
-      this.urls.default = urlOrConfig;
-    }
-
-    if (angular.isObject(urlOrConfig)) {
-      this.urls = urlOrConfig;
-    }
-
-  };
-
-  this.$$checkUrls = function $$checkUrls(urlConfig) {
-    if (!urlConfig) {
-      return new Error('No Firebase URL registered. Use firebaseRefProvider.registerUrl() in the config phase to set up a root reference.');
-    }
-  };
-
-  this.$$createRefsFromUrlConfig = function $$createMultipleRefs(urlConfig) {
-    var error = this.$$checkUrls(urlConfig);
-    if (error) { throw error; }
-    var defaultUrl = urlConfig.default;
-    var defaultRef = new Firebase(defaultUrl);
-    delete urlConfig.default;
-    angular.forEach(urlConfig, function(value, key) {
-      if (!defaultRef.hasOwnProperty(key)) {
-        defaultRef[key] = new Firebase(value);
-      } else {
-        throw new Error(key + ' is a reserved property name on firebaseRef.');
+      if (typeof urlOrConfig === 'string') {
+        this.urls = {};
+        this.urls.default = urlOrConfig;
       }
-    });
-    return defaultRef;
-  };
 
-  this.$get = function FirebaseRef_$get() {
-    return this.$$createRefsFromUrlConfig(this.urls);
-  };
-}
+      if (angular.isObject(urlOrConfig)) {
+        this.urls = urlOrConfig;
+      }
 
-angular.module('firebase')
-  .provider('firebaseRef', FirebaseRef);
+    };
+
+    this.$$checkUrls = function $$checkUrls(urlConfig) {
+      if (!urlConfig) {
+        return new Error('No Firebase URL registered. Use firebaseRefProvider.registerUrl() in the config phase to set up a root reference.');
+      }
+    };
+
+    this.$$createRefsFromUrlConfig = function $$createMultipleRefs(urlConfig) {
+      var error = this.$$checkUrls(urlConfig);
+      if (error) { throw error; }
+      var defaultUrl = urlConfig.default;
+      var defaultRef = new Firebase(defaultUrl);
+      delete urlConfig.default;
+      angular.forEach(urlConfig, function(value, key) {
+        if (!defaultRef.hasOwnProperty(key)) {
+          defaultRef[key] = new Firebase(value);
+        } else {
+          throw new Error(key + ' is a reserved property name on firebaseRef.');
+        }
+      });
+      return defaultRef;
+    };
+
+    this.$get = function FirebaseRef_$get() {
+      return this.$$createRefsFromUrlConfig(this.urls);
+    };
+  }
+
+  angular.module('firebase')
+    .provider('firebaseRef', FirebaseRef);
+    
+})();
