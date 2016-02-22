8132ec9b512da02a0db7b07f952e6039a5aef3b9
David East <david@firebase.com>
2016-02-22 09:55:34 -0800

    $firebaseAuthService unit tests

---
+++ tests/unit/FirebaseAuthService.spec.js
@@ -0,0 +1,27 @@
+'use strict';
+describe('$firebaseAuthService', function () {
+  var firebaseRefProvider;
+  var MOCK_URL = 'https://stub.firebaseio-demo.com/'
+
+  beforeEach(module('firebase', function(_firebaseRefProvider_) {
+    firebaseRefProvider = _firebaseRefProvider_;
+    firebaseRefProvider.registerUrl(MOCK_URL);
+  }));
+
+  describe('<constructor>', function() {
+
+    var $firebaseAuthService;
+    beforeEach(function() {
+      module('firebase');
+      inject(function (_$firebaseAuthService_) {
+        $firebaseAuthService = _$firebaseAuthService_;
+      });
+    });
+
+    it('should exist based on using firebaseRefProvider.registerUrl()', inject(function() {
+      expect($firebaseAuthService).not.toBe(null);
+    }));
+    
+  });
+
+});
