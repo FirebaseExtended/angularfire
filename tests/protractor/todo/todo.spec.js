794af7a73383c53ac38ca185e25bdf08ce7883bd
abehaskins <abeisgreat@abeisgreat.com>
2016-05-18 15:40:17 -0700

    Hack in initializeApp for e2e tests

--- tests/protractor/todo/todo.spec.js
+++ tests/protractor/todo/todo.spec.js
@@ -1,6 +1,14 @@
 var protractor = require('protractor');
 var firebase = require('firebase');
 
+var config = {
+  apiKey: "AIzaSyCcB9Ozrh1M-WzrwrSMB6t5y1flL8yXYmY",
+  authDomain: "angularfire-dae2e.firebaseapp.com",
+  databaseURL: "https://angularfire-dae2e.firebaseio.com",
+  storageBucket: "angularfire-dae2e.appspot.com",
+};
+firebase.initializeApp(config);
+
 describe('Todo App', function () {
   // Reference to the Firebase which stores the data for this demo
   var firebaseRef = firebase.database().ref();
