440db1a53b88b11be6f2e9f4945f836f0b5e5e39
David East <deast@google.com>
2016-08-16 14:17:42 -0700

    feat(storage): Add bindings for Firebase Storage

---
+++ src/storage/FirebaseStorage.js
@@ -0,0 +1,78 @@
+(function() {
+
+  function FirebaseStorage() {
+
+    return function FirebaseStorage(storageRef) {
+      _assertStorageRef(storageRef);
+      return {
+        $put: function $put(file) {
+          return _$put(storageRef, file);
+        },
+        $getDownloadURL: function $getDownloadURL() {
+          return _$getDownloadURL(storageRef);
+        }
+      };
+    };
+  }
+
+  FirebaseStorage._ = {
+    _unwrapStorageSnapshot: unwrapStorageSnapshot,
+    _$put: _$put,
+    _$getDownloadURL: _$getDownloadURL,
+    _isStorageRef: isStorageRef,
+    _assertStorageRef: _assertStorageRef
+  };
+
+  function unwrapStorageSnapshot(storageSnapshot) {
+    return {
+      bytesTransferred: storageSnapshot.bytesTransferred,
+      downloadURL: storageSnapshot.downloadURL,
+      metadata: storageSnapshot.metadata,
+      ref: storageSnapshot.ref,
+      state: storageSnapshot.state,
+      task: storageSnapshot.task,
+      totalBytes: storageSnapshot.totalBytes
+    };
+  }
+
+  function _$put(storageRef, file) {
+    var task = storageRef.put(file);
+
+    return {
+      $progress: function $progress(callback) {
+        task.on('state_changed', function (storageSnap) {
+          return callback(unwrapStorageSnapshot(storageSnap));
+        }, function (err) {}, function (storageSnap) {});
+      },
+      $error: function $error(callback) {
+        task.on('state_changed', function (storageSnap) {}, function (err) {
+          return callback(err);
+        }, function (storageSnap) {});
+      },
+      $complete: function $complete(callback) {
+        task.on('state_changed', function (storageSnap) {}, function (err) {}, function (_) {
+          return callback(unwrapStorageSnapshot(task.snapshot));
+        });
+      }
+    };
+  }
+
+  function _$getDownloadURL(storageRef) {
+    return storageRef.getDownloadURL();
+  }
+
+  function isStorageRef(value) {
+    value = value || {};
+    return typeof value.put === 'function';
+  }
+
+  function _assertStorageRef(storageRef) {
+    if (!isStorageRef(storageRef)) {
+      throw new Error('$firebaseStorage expects a storage reference from firebase.storage().ref()');
+    }
+  }
+
+  angular.module('firebase.storage')
+    .factory('$firebaseStorage', FirebaseStorage);
+
+})();
