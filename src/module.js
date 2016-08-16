0cd6413774603c5cf9565aeab6aa5155a3ef025f
Abraham Haskins <abeisgreat@abeisgreat.com>
2016-08-16 12:00:48 -0700

    Makes $firebase module hold everything

--- src/module.js
+++ src/module.js
@@ -1,14 +1,14 @@
 (function(exports) {
   "use strict";
 
-  // Define the `firebase` module under which all AngularFire
-  // services will live.
-  angular.module("firebase", [])
-    //TODO: use $window
-    .value("Firebase", exports.Firebase);
-
   angular.module("firebase.utils", []);
   angular.module("firebase.config", []);
   angular.module("firebase.auth", ["firebase.utils"]);
   angular.module("firebase.database", ["firebase.utils"]);
+
+  // Define the `firebase` module under which all AngularFire
+  // services will live.
+  angular.module("firebase", ["firebase.utils", "firebase.config", "firebase.auth", "firebase.database"])
+    //TODO: use $window
+    .value("Firebase", exports.Firebase);
 })(window);
