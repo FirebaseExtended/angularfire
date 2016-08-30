fed79f814ac0de2345c1e3edd7c9396c531c871e
jwngr <jacob@firebase.com>
2016-08-29 22:58:34 -0700

    Remove need for service account to run tests

--- tests/initialize-node.js
+++ tests/initialize-node.js
@@ -1,14 +1,6 @@
 var path = require('path');
 var firebase = require('firebase');
 
-if (!process.env.ANGULARFIRE_TEST_DB_URL) {
-  throw new Error('You need to set the ANGULARFIRE_TEST_DB_URL environment variable.');
-}
-
-try {
-  firebase.initializeApp({
-    databaseURL: process.env.ANGULARFIRE_TEST_DB_URL
-  });
-} catch (err) {
-  console.log('Failed to initialize the Firebase SDK [Node]:', err);
-}
+firebase.initializeApp({
+  databaseURL: 'https://oss-test.firebaseio.com'
+});
--- tests/initialize.js
+++ tests/initialize.js
@@ -2,14 +2,10 @@ if (window.jamsine) {
   jasmine.DEFAULT_TIMEOUT_INTERVAL = 5000;
 }
 
-try {
-  // TODO: stop hard-coding this
-  var config = {
-    apiKey: "AIzaSyCcB9Ozrh1M-WzrwrSMB6t5y1flL8yXYmY",
-    authDomain: "angularfire-dae2e.firebaseapp.com",
-    databaseURL: "https://angularfire-dae2e.firebaseio.com"
-  };
-  firebase.initializeApp(config);
-} catch (err) {
-  console.log('Failed to initialize the Firebase SDK [web]:', err);
-}
+var config = {
+  apiKey: 'AIzaSyC3eBV8N95k_K67GTfPqf67Mk1P-IKcYng',
+  authDomain: 'oss-test.firebaseapp.com',
+  databaseURL: 'https://oss-test.firebaseio.com',
+  storageBucket: 'oss-test.appspot.com'
+};
+firebase.initializeApp(config);
